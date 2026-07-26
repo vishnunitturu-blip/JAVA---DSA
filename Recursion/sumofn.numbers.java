e8eae2f30abdeeaaf16cc087891cdbd85cac9728
vishnu <vishnunitturu@gmail.com>
2026-07-26 22:53:21 +0530

    recursion sum of n numbers

---
+++ Recursion/sumofn.numbers.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+     static int sum(int n){
+            if(n==0){
+                return 0;
+            }
+         return n+sum(n-1);
+     }
+
+    public static void main(String[] args) {
+        Scanner z=new Scanner(System.in);
+        int n=z.nextInt();
+        System.out.println("Sum : "+sum(n));
+    }
+}
