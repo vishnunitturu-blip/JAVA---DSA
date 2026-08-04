3e3459d4506395c9dd72b151aa80c7830252a293
vishnu <vishnunitturu@gmail.com>
2026-08-04 23:09:55 +0530

    Fun - Multiplication Table

---
+++ fun/table.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+Scanner z=new Scanner(System.in);
+        int n=z.nextInt();
+        int sum=0;
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" x "+i+" = "+n*i);
+        }
+            
+        
+    }
+}
