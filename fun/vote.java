d5766adf401445e051cb26b6ce77ff8a7fd5b0c3
Nitturu vishnu <vishnunitturu@gmail.com>
2026-08-09 22:39:56 +0530

    Add voting eligibility check in vote.java

---
+++ fun/vote.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner z=new Scanner(System.in);
+        int n=z.nextInt();
+        if (n>=18){
+            System.out.println("Eligible");
+        }
+        else{
+System.out.println("Not Eligible");
+        }
+    }
+}
