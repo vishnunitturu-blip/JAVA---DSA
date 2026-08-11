6b4bcb62a08aa6227ff0a675ac57811bf45e042e
Nitturu vishnu <vishnunitturu@gmail.com>
2026-08-11 21:53:53 +0530

    Add simple interest calculation in intrest.java

---
+++ fun/intrest.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+Scanner z=new Scanner(System.in);
+        int P=z.nextInt();
+        int R=z.nextInt();
+        int T=z.nextInt();
+        int SI=P*R*T;
+        int res=SI/100;{
+            System.out.println(res);
+        }
+    }
+}
