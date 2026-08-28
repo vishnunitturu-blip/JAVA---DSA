366cc984048e3ad5b3b00a7a97589f6379ece302
Nitturu vishnu <vishnunitturu@gmail.com>
2026-08-28 23:03:18 +0530

    Create inverted triangle.java

---
+++ fun/inverted triangle.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner z=new Scanner(System.in);
+        int n=z.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if (j<=n-i+1) {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
