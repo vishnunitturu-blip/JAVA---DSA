31a7f4c71e42abe359c2831a22926a3d895f2565
vishnu <vishnunitturu@gmail.com>
2026-09-02 23:17:57 +0530

    DP - Longest Palindromic Subsequence

---
+++ DP/palindromic.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner z=new Scanner(System.in);
+        String s=z.nextLine();
+        String t=new StringBuilder(s).reverse().toString();
+        int n=s.length();
+        int m=t.length();
+        int [][]arr=new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                    if(s.charAt(i-1)==t.charAt(j-1)){
+                        arr[i][j]=arr[i-1][j-1]+1;
+                    }
+                else{
+                    arr[i][j]=Math.max(arr[i][j-1],arr[i-1][j]);
+                }
+                }
+        }
+        System.out.println(arr[n][m]);
+    }
+}
