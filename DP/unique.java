6bf0b1fbb1b99eece0496a0d5d2e7246942358ac
Nitturu vishnu <vishnunitturu@gmail.com>
2026-09-05 22:11:07 +0530

    Add unique.java for unique paths calculation
    Implement dynamic programming solution to find unique paths in a grid.

---
+++ DP/unique.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner z=new Scanner(System.in);
+        int n=z.nextInt();
+        int m=z.nextInt();
+        int [][]dp=new int[n][m];
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+            for(int j=0;j<m;j++){
+                dp[0][j]=1;
+            }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        System.out.println(dp[n-1][m-1]);
+            }
+}
